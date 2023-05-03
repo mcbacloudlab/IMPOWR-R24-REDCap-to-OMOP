19d31c03f6e5176589263e245a88fefdc844b65d
Matt Perkins <mlperkin@wakehealth.edu>
2023-05-03 10:54:00 -0400

    increase event listeners to 20 from 10

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,12 @@ const { createBullBoard } = require("bull-board");
 const { BullAdapter } = require("bull-board/bullAdapter");
 const basicAuth = require('express-basic-auth');
 const syncRedisAndJobDB = require('./gpt3/syncRedisAndJobDB');
+const EventEmitter = require('events'); // Import the EventEmitter class from the 'events' module
+const commander = new EventEmitter(); // Create a new instance of EventEmitter
+
 // const _redisServer = require("redis-server");
+
+commander.setMaxListeners(20); // Increase the limit to 20
 
 // Sync redis and mysql job ids
 setInterval(syncRedisAndJobDB, 60000);
