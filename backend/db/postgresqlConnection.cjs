1c3fd2c237a975449d74e517e27bcd294dab79d1
Matt Perkins <mlperkin@wakehealth.edu>
2023-05-04 10:32:36 -0400

    code cleanup, testing backend pipeline

--- backend/db/postgresqlConnection.cjs
+++ backend/db/postgresqlConnection.cjs
@@ -8,16 +8,9 @@ const credentials = {
     port: 5432,
   };
 console.log("Connecting to PostgreSQL DB" + new Date().toLocaleString())
-// create the mysql_pool to database
 // Connect with a connection pool.
-
-// async function poolDemo() {
 const pg_pool = new Pool(credentials);
-    // const now = await pool.query("SELECT NOW()");
-    // await pool.end();
-  
-    // return now;
-  // }
+
 console.log('Connected to PostgreSQL DB at ' + new Date().toLocaleString())
 
 module.exports = pg_pool
